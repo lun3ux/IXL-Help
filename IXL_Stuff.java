6eea702bcab7625bbdccd713c945257ab1697339
Luneux <yanselpedraza@gmail.com>
2025-01-31 17:58:33 -0600

    Add files via upload
    minor changes

--- IXL_Stuff.java
+++ IXL_Stuff.java
@@ -1,124 +1,66 @@
 import java.util.ArrayList;
-import java.util.Scanner;
-
 
 public class IXL_Stuff {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("0 = Check If there is a right Triangle");
-        System.out.println("1 = Find Hypotenuse");
-        System.out.println("2 = Find Leg");
-
-        int control = sc.nextInt();
-        switch(control) {
-            case 0:
-            pythageoreanisrighttrangle(args);
-                break;
-            case 1:
-            pythageoreantheorem(args);
-            break;
-            case 2:
-            missingleg(args);
-            default:
-            System.out.println("");
-            sc.close();
-        
-        }
-
     }
-    public static void pythageoreanisrighttrangle(String[] args) {
+    public static boolean pythageoreanisrighttrangle(double a, double b, double c) {
 
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> numbers = new ArrayList<>();
-        System.out.println("Insert First Digit");
-        int a = sc.nextInt();
-        System.out.println("Insert Second Digit");
-        int b = sc.nextInt();
-        System.out.println("Insert Third Digit");
-        int c = sc.nextInt();
+        int x;
+        int y;
+        int z;
 
-        numbers.add(a);
-        numbers.add(b);
-        numbers.add(c);
+            ArrayList<Integer> numbers = new ArrayList<>();
 
-
-        int largest = numbers.get(0);
-
-        for (int i = 0; i < 3; i++) {
-            if (numbers.get(i) > largest) {
-                largest = numbers.get(i);
-                
-        }
-        numbers.remove(numbers.indexOf(largest));
-
-        System.out.println(numbers);
-        System.out.println("The largest is " + largest);
-        numbers.add(largest);
-        }
-        int x = numbers.get(0) * numbers.get(0);
-        int y = numbers.get(1) * numbers.get(1);
-        int z = largest * largest;
-        System.out.println(x);
-        System.out.println(y);
-        System.out.println(z);
-        sc.close(); 
-
-        if(x+y==z) {
-            System.out.println("true");
-        }
-        else {
-            System.out.println("false");
-        }
-
+            numbers.add((int) a);
+            numbers.add((int) b);
+            numbers.add((int) c);
+            int largest = numbers.get(0);
+            for (int i = 0; i < 3; i++) {
+                if (numbers.get(i) > largest) {
+                    largest = numbers.get(i);
+                    
+                }
+                numbers.remove(numbers.indexOf(largest));
+            
+                numbers.add(largest);
+            }   x = numbers.get(0) * numbers.get(0);
+            y = numbers.get(1) * numbers.get(1);
+            z = largest * largest;
+        return x+y==z;
     }
    
-    public static void pythageoreantheorem(String[] args) {
+    public static double pythageoreantheorem(double a, double b) {
+            ArrayList<Integer> numbers = new ArrayList<>();
 
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> numbers = new ArrayList<>();
-        System.out.println("Insert First Digit");
-        int a = sc.nextInt();
-        System.out.println("Insert Second Digit");
-        int b = sc.nextInt();
-        
-        numbers.add(a);
-        numbers.add(b);
-        int x = numbers.get(0)* numbers.get(0);
-        int y = numbers.get(1)* numbers.get(1);
-        int z = x+y;
-        double w = Math.sqrt(z);
-        System.out.println(w);
-        sc.close();
+            numbers.add((int) a);
+            numbers.add((int) b);
+
+            double x = numbers.get(0)* numbers.get(0);
+            double y = numbers.get(1)* numbers.get(1);
+            double z = x+y;
+            double w = Math.sqrt(z);
+            return w;
     }
   
-    public static void missingleg(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> numbers = new ArrayList<>();
-        System.out.println("Insert First Digit");
-        int a = sc.nextInt();
-        System.out.println("Insert Second Digit");
-        int b = sc.nextInt();
+    public static double missingleg(double a,double b) {
+        ArrayList<Double> numbers = new ArrayList<>();
 
         numbers.add(a);
         numbers.add(b);
 
-        int largest = numbers.get(0);
+        double largest = numbers.get(0);
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 1; i++) {
             if (numbers.get(i) > largest) {
-                largest = numbers.get(i);
-                
+                largest = numbers.get(i);      
         }
-        numbers.remove(numbers.indexOf(largest));
 
-        System.out.println(numbers);
-        System.out.println("The largest is " + largest);
-        
-        int x = numbers.get(0)* numbers.get(0);
-        int z = largest-x;
+        }
+        numbers.remove(numbers.indexOf(largest));      
+        int x = (int) (numbers.get(0)* numbers.get(0));
+        double z = largest-x;
         double w = Math.sqrt(z);
-        System.out.println(w);
-        sc.close();
+        return w;
     }
-    }
+    
 }
